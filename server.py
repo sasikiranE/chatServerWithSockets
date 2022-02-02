86b9e5791925801658b0aea4db2265f7bed4c15f
sasikiran <sasikiranessamaraju@gmail.com>
2022-02-02 17:18:47 +0530

    complete chatroom using sockets.

--- server.py
+++ server.py
@@ -1,17 +1,82 @@
 import socket
+import select
 
-# create a INET, STREAMING socket.
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HEADER_LENGTH = 10
 
-# bind the socket to public host, and port 1234.
-serversocket.bind((socket.gethostname(), 1234))
+IP = "127.0.0.1"
+PORT = 1234
 
-# queue up as many as 5 connection requests before refusing outside connections.
-serversocket.listen(5)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+print(f'Listening for connections on {IP}:{PORT}...')
+
+def receive_message(client_socket):
+
+    try:
+
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode('utf-8').strip())
+
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+
+        return False
 
 while True:
-    # accept connections from outside.
-    (clientsocket, address) = serversocket.accept()
-    print(f"Connection from {address} has been established!")
-    welcome_msg = "Welcome to the server!"
-    clientsocket.send(welcome_msg.encode())
+
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = receive_message(client_socket)
+
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+
+        else:
+
+            message = receive_message(notified_socket)
+
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+
+                sockets_list.remove(notified_socket)
+
+                del clients[notified_socket]
+
+                continue
+
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+
+        del clients[notified_socket]
